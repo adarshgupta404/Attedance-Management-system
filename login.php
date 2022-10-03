2224211f85798ec193b0d65d48aafa5c4b188d00
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2022-10-03 20:43:03 +0530

    Update login.php

--- login.php
+++ login.php
@@ -8,3 +8,4 @@
 	$student_name = $f_student['firstname']." ".$f_student['lastname'];
 	$conn->query("INSERT INTO `time` VALUES('', '$student', '$student_name', '$time', '$date')") or die(mysqli_error());
 	echo "<h3 class = 'text-muted'>".$student_name." <label class = 'text-info'>at  ".date("h:i a", strtotime($time))."</label></h3>";
+?>
